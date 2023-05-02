9b61497483486f5476f70677258ced5712feece8
Cooper <cooper.softdev@gmail.com>
2023-05-02 14:30:25 -0800

    commented out code to make writing easier

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 'use strict';
 
 console.log();
-let userName = prompt('What is your name?');
+// let userName = prompt('What is your name?');
 
-alert(`Welcome to the shitshow ${userName} let's answer some questions.`);
+// alert(`Welcome to the shitshow ${userName} let's answer some questions.`);
 
-let questionOneGuess = prompt('Am I retarded?');
-if(questionOneGuess === 'y' || questionOneGuess === 'yes'){
-  alert('You are correct!');
-}
+// let questionOneGuess = prompt('?');
+// if(questionOneGuess === 'y' || questionOneGuess === 'yes'){
+//   alert('You are correct!');
+// }
